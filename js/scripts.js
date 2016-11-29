c79e7a45cc6d0a0042073ae180192d0097e9b4b7
Justin Leatherwood <jplwood@gmail.com>
2016-11-28 22:37:25 -0600

    fixed things, updated info

--- js/scripts.js
+++ js/scripts.js
@@ -26,22 +26,6 @@ $(document).ready(function() {
     });
   }
 
-  // Ease anchor scrolling - credit to Chris Coyier & css-tricks
-  $(function() {
-    $('a[href*="#"]:not([href="#"])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-          $('html, body').animate({
-            scrollTop: target.offset().top
-          }, 1000);
-          return false;
-        }
-      }
-    });
-  });
-
   //Increase Quote Opacity on Scroll
   $(window).scroll(function () {
     var scrollTop = $(window).scrollTop();
