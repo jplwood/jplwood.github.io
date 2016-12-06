73e9d5e0b978590d6b5085b78f47b6b0f5b7fe5d
Justin Leatherwood <jplwood@gmail.com>
2016-12-05 20:12:19 -0600

    cut out quotes.js, added new ones

--- js/scripts.js
+++ js/scripts.js
@@ -45,11 +45,13 @@ $(document).ready(function() {
 
 function setRandomQuote() {
   var randomQuote = quotes[Math.floor(Math.random()*quotes.length)];
-  $('.quote').html(randomQuote.quote);
+  var quoteHTML = randomQuote.quote.length > 140 ? "<small>" + randomQuote.quote + "</small>" : randomQuote.quote;
+  $('.quote').html(quoteHTML);
   $('.author').html(randomQuote.author);
   if(randomQuote.note==="") {
-    $('.note').remove();
+    $('.note').hide();
   } else {
+    $('.note').show();
     $('.note').html(randomQuote.note);
   }
 }
@@ -57,3 +59,185 @@ function setRandomQuote() {
 function removeHash () {
     history.pushState("", document.title, window.location.pathname + window.location.search);
 }
+
+
+
+// Data
+var quotes = [
+  {
+    "quote": "Instructions for living a life. Pay attention. Be astonished. Tell about it.",
+    "author": "Mary Oliver",
+    "note":""
+  },
+  {
+    "quote": "What does a rock know of April?",
+    "author": "Rumi",
+    "note":""
+  },
+  {
+    "quote": "I loathe, I abhor the sea, & all ships which sail upon it.",
+    "author": "Charles Darwin",
+    "note": "1836, nearing the end of his voyage"
+  },
+  {
+    "quote": "Progress is the soul of the Universe",
+    "author": "Pierre Teilhard de Chardin",
+    "note": ""
+  },
+  {
+    "quote": "What shall I learn of beans or beans of me?",
+    "author": "Henry David Thoreau",
+    "note": "when planting a field of beans"
+  },
+  {
+    "quote": "What are these pines & these birds about? What is this pond a-doing? I must know a little more.",
+    "author": "Henry David Thoreau",
+    "note": "on struggling to finish his Walden manuscript"
+  },
+  {
+    "quote": "A cask of wine works more miracles than a church full of saints.",
+    "author": "Italian Proverb",
+    "note": ""
+  },
+  {
+    "quote": "I omit the unusual - the hurricanes and earthquakes - and describe the common, this is the true theme of poetry.",
+    "author": "Henry David Thoreau",
+    "note": ""
+  },
+  {
+    "quote": "How glorius these sunbeams are! They seem to call Earth to the Heavens!",
+    "author": "Alexander von Humbolt",
+    "note": "1859, on the day of his death"
+  },
+  {
+    "quote": "When we try to pick anything out by itself, we find it hitched to everything else in the universe.",
+    "author": " John Muir",
+    "note": "My First Summer in the Sierra"
+  },
+  {
+    "quote": "...no doubt these trees would make good lumber after passing through a sawmill, as George Washington after passing through the hands of a French cook would have made good food.",
+    "author": "John Muir",
+    "note": "upon seeing Sequoias felled for timber"
+  },
+  {
+    "quote": "I only went out for a walk, and finally concluded to stay out til sundown, for going out, I found, was really going in.",
+    "author": "John Muir",
+    "note": ""
+  },
+  {
+    "quote": "There is in fact no distinction between the fate of the land and the fate of the people. When one is abused the other suffers.",
+    "author": "Wendell Berry",
+    "note": ""
+  },
+  {
+    "quote": "I have no plan, system or trick... I mean simply to go on hammering & thumping as best I can.",
+    "author": "John Muir",
+    "note": "on his desire to keep nature pristine"
+  },
+  {
+    "quote": "Anyone who takes himself too seriously always runs the risk of looking ridiculous; anyone who can consistently laugh at himself does not.",
+    "author": "Vaclav Havel",
+    "note": ""
+  },
+  {
+    "quote": "Rivers know this: There is no hurry. We shall get there someday.",
+    "author": "A. A. Milne",
+    "note": "author of Winnie-the-Pooh"
+  },
+  {
+    "quote": "The more original a discovery, the more obvious it seems afterwards.",
+    "author": "Arthur Koestler",
+    "note": "novelist"
+  },
+  {
+    "quote": "It is not enough to be busy. So are the ants. The question is, what are we busy about.",
+    "author": "Henry David Thoreau",
+    "note": ""
+  },
+  {
+    "quote": "What an elder sees sitting, the young can't see standing",
+    "author": "Gustave Flaubert",
+    "note": ""
+  },
+  {
+    "quote": "No one has ever become poor by giving.",
+    "author": "Anne Frank",
+    "note": ""
+  },
+  {
+    "quote": "Don't be afraid of missing opportunities. Behind every failure is an opportunity somebody wishes they had missed.",
+    "author": "Lily Tomlin",
+    "note": ""
+  },
+  {
+    "quote": "There are two kinds of writer's block. One is when you freeze because you think you can't do it. The other is when you think it's not worth doing.",
+    "author": "Tom Wolfe",
+    "note": ""
+  },
+  {
+    "quote": "Advice is judged by results, not by intentions.",
+    "author": "Cicero",
+    "note": ""
+  },
+  {
+    "quote": "Stories mimic life like certain insects mimic leaves and twigs.",
+    "author": "Mary Gaitskill",
+    "note": ""
+  },
+  {
+    "quote": "Not everything that is faced can be changed, but nothing can be changed until it is faced.",
+    "author": "James Baldwin",
+    "note": ""
+  },
+  {
+    "quote": "My exit is the result of too many entrées.",
+    "author": "Richard Monckton Milnes",
+    "note": ""
+  },
+  {
+    "quote": "...we [humans] frequently mistake comfort for well-being.",
+    "author": "Daniel Lieberman",
+    "note": ""
+  },
+  {
+    "quote": "...your body is not the best of all possible bodies. But it is the only one you'll ever have, and it's worith enjoying, nurturing, and protecting.",
+    "author": "Daniel Lieberman",
+    "note": ""
+  },
+  {
+    "quote": "Doing something joyfully doesn't make it any easier, it only makes it better - and it also makes it communal - that we're all doing it together. When you work in fear, or if you work in distress, you often feel alone.",
+    "author": "Stephen Colbert",
+    "note": ""
+  },
+  {
+    "quote": "Everything that is interesting is 90% boring.",
+    "author": "Elizabeth Gilbert",
+    "note": "quoted from <a href='http://www.onbeing.org/program/elizabeth-gilbert-choosing-curiosity-over-fear/8786'>On Being</a>"
+  },
+  {
+    "quote": "A true vocation always metamorphosizes both ambition and failure into compassion and understanding for others",
+    "author": "David Whyte",
+    "note": ""
+  },
+  {
+    "quote": "Good developers know how things work. Great developers know why things work.",
+    "author": "adage",
+    "note": ""
+  },
+  {
+    "quote": "We're programmers. Programmers are, in their hearts, architects, and the first thing they want to do when they get to a site is to bulldoze the place flat and build something grand. We're not excited by incremental renovation: tinkering, improving, planting flower beds.",
+    "author": "Joel Spolsky",
+    "note": "<a href='http://www.joelonsoftware.com/articles/fog0000000069.html'>Things You Should Never Do, Part I</a>"
+  },
+];
+
+/*
+
+For extra quotes
+
+{
+  "quote": "",
+  "author": "",
+  "note": ""
+},
+*/
